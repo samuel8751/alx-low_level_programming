ff18d8fe448fb575dfd2c8730b7d188e4a92a61d
samuel8751 <samuelgithinji041@gmail.com>
2023-07-21 19:16:20 -0700

    largest factor

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,30 +1,24 @@
 #include <stdio.h>
 
 /**
-* main - Entry point of the program
-*
-* Description: Causes an infinite loop
-*
-* Return: Always 0 (Success)
+* main - causes an infinite loop
+* Return: 0
 */
+
 int main(void)
-{
-	int i;
-
-	printf("Infinite loop incoming :(\n");
-
-	i = 0;
-
-	/*
-	 * Commented out the infinite loop
-	 *  while (i < 10)
 	{
-		putchar(i);
-	}
-	*/
+int i;
 
-	printf("Infinite loop avoided! \\o/\n");
+printf("Infinite loop incoming :(\n");
+i = 0;
 
-	return (0);
+/*
+* while (i < 10)
+{
+putchar(i);
+}
+*/
+printf("Infinite loop avoided! \\o/\n");
+return (0);
 }
 
