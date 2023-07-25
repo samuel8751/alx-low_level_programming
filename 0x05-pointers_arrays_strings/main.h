44f5ee3b2d485808d33fdca09ab84c0b593d31e7
samuel8751 <samuelgithinji041@gmail.com>
2023-07-24 19:51:52 -0700

    pointers and arrays questions

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,5 +6,9 @@ void _puts(char *str);
 int _strlen(char *s);
 void print_rev(char *s);
 char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
+int _putchar(char c);
 void rev_string(char *s);
 #endif
