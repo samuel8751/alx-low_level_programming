f5a288a2e21d4f6c6a43751e9dd4ae9224cb1bf1
samuel8751 <samuelgithinji041@gmail.com>
2023-07-31 13:29:13 -0700

    more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strstr - locates a substring
  * @haystack: the string to be searched
@@ -7,22 +8,24 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-int index;
-if (*needle == 0)
-return (haystack);
-while (*haystack)
-{
-index = 0;
-if (haystack[index] == needle[index])
-{
-do {
-if (needle[index + 1] == '\0')
-return (haystack);
-index++;
+	int index;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		index = 0;
+		if (haystack[index] == needle[index])
+		{
+			do {
+				if (needle[index + 1] == '\0')
+					return (haystack);
+				index++;
+			} while (haystack[index] == needle[index]);
+		}
+		haystack++;
+	}
+	return ('\0');
 }
-while (haystack[index] == needle[index])
-}
-haystack++;
-}
-return ('\0');
-}
+
