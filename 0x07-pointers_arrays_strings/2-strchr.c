64bbb54c60089098b93deeb9e45bf694f50ab170
samuel8751 <samuelgithinji041@gmail.com>
2023-07-31 13:34:26 -0700

    more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _strchar - locates a character in a string
+ * _strchr - locates a character in a string
  * @s: the string to be searching
  * @c: the character to locate
  * Return: pointer to occurrence
  */
-char *_strchar(char *s, char c)
+char *_strchr(char *s, char c)
 {
 int index;
 for (index = 0; s[index] >= '\0'; index++)
