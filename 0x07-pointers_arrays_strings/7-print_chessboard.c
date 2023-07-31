0ef3c6676f014ac4a94a6713aaad8472b0126474
samuel8751 <samuelgithinji041@gmail.com>
2023-07-31 13:21:14 -0700

    more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,24 +1,18 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * print_diagsum - prints the sum of two diagnals  of a square matrix
- * @a: the matrix of int
- * @size: matrix size
+ * print_chessboard - prints the chessboard
+ * @a: pointer to piece to print
  * Return: void
  */
-void print_diagsum(int *a, int size)
+void print_chessboard(char (*a)[8])
 {
-int index, sum1 = 0, sum2  = 0;
-for (index = 0; index < size; index++)
+int i, j;
+for (i = 0; i < 8; i++)
 {
-sum1 += a[index];
-a += size;
+for (j = 0; j < 8; j++)
+{
+_putchar(a[i][j]);
 }
-a -= size;
-for (index = 0; index < size; index++)
-{
-sum2 += a[index];
-a -= size;
+_putchar('\n');
 }
-printf("%d, %d\n", sum1, sum2);
 }
