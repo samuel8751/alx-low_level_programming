7e164d41c2609626df13aa7ab1d4de09a4bc5def
samuel8751 <samuelgithinji041@gmail.com>
2023-08-02 18:37:23 -0700

    0x08. C - Recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,11 +7,12 @@
  */
 int _strlen_recursion(char *s)
 {
+int length;
 if (*s == '\0')
 {
 return (0);
 }
-int length = _strlen_recursion(s + 1);
+length = _strlen_recursion(s + 1);
 return (length + 1);
 }
 
