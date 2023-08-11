a9e1462895d006efed0f6059234119df506435b4
samuel8751 <samuelgithinji041@gmail.com>
2023-08-10 19:16:17 -0700

    0x0C. C - More malloc, free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,39 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
- * string_nconcat - concantinates two strings
- * @s1: string one
- * @s2: string two
- * @n: number of elements to concantinate
- * Return: pointer
+ * string_nconcat - Concatenates two strings.
+ * @s1: String one.
+ * @s2: String two.
+ * @n: Number of elements to concatenate.
+ *
+ * Return: Pointer to the concatenated string, NULL if fails.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-size_t i, j, k;
-char *s;
-if (s1 == NULL)
-i = 0;
-{
-for (i = 0; s1[i] != '\0'; i++)
-;
+	size_t i, j, k;
+	char *s;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+
+	for (j = 0; s2[j] != '\0' && j < n; j++)
+		;
+
+	s = malloc(sizeof(char) * (i + j + 1));
+	if (s == NULL)
+		return (NULL);
+
+	for (k = 0; k < i; k++)
+		s[k] = s1[k];
+	for (k = 0; k < j; k++)
+		s[k + i] = s2[k];
+	s[i + j] = '\0';
+
+	return (s);
 }
-if (s2 == NULL)
-j = 0;
-else
-{
-for (j = 0; s2[j] != '\0'; j++)
-;
-}
-if (j > n)
-j = n;
-s = malloc(sizeof(char) * (i + j + 1));
-if (s == NULL)
-return (NULL);
-for (k = 0; k < i; k++)
-s[k] = s1[k];
-for (k = 0; k < j; k++)
-s[k + i] = s2[k];
-s[i + j] = '\0';
-return (s);
-}
+
