62a8ef46281bd27bdf4664f26f68311df0fedebb
samuel8751 <samuelgithinji041@gmail.com>
2023-08-10 19:53:42 -0700

    0x0C. C - More malloc, free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,49 +1,104 @@
-#include "main.h"                                          #include <stdio.h>                                         #include <stdlib.h>                                        #define ERROR "Error"                                      /**                                                         * is_digit - check if a string contains nob-digit char     * @s: string to evaluate                                   * Return: 0(succesd) 1 (error)                             */                                                        int is_digit(char *s)                                      {
-int i = 0;                                                 while (s[i])                                               {                                                          if (s[i] < '0' || s[i] > '9')                              return (0);
-i++:
-}                                                          return (1);                                                }                                                          /**                                                         * _strlen -creturns the len of the string                  * @s: string to evaluate                                   * Return: length of the string                             */                                                        int _strlen(char *s)                                       {
-int i = 0;
-while (s[i] != '\0')
-{                                                          i++;
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+    int i = 0;
+
+    while (s[i] != '\0')
+    {
+        i++;
+    }
+
+    return i;
 }
-return (i);
+
+
+/**
+ * is_digit - check if a string contains only digit characters
+ * @s: string to evaluate
+ * Return: 1 (if all characters are digits), 0 (otherwise)
+ */
+int is_digit(char *s)
+{
+    while (*s)
+    {
+        if (!isdigit(*s))
+            return (0);
+        s++;
+    }
+    return (1);
 }
+
 /**
  * _error - handle error
- * Return: void
  */
 void _error(void)
 {
-printf("Error\n");
-{
-printf("Error\n");
-exit (98);
+    printf("Error\n");
+    exit(98);
 }
 
 /**
- * main - multiplies 2 numbers
- * @argc: arg count
- * @argv: arg vector
- * Return: 0
+ * main - multiplies two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 98 on error
  */
 int main(int argc, char *argv[])
 {
-char *s1, s2;
-int len1, len2, i, len, carry, digit1, digit2, *results, a;
+    char *s1, *s2;
+    int len1, len2, i, len, carry, digit1, digit2, *result, a = 0;
 
-s1 = argv[1];
-s2 = argv[2];
-if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-error ();
-len1 = _strlen(s1);
-len2 = _strlen(s2);
-len = len1 + len2 + 1;
-result = mslloc(sizeof(int) * len);
-if (!result)                                               return (1);
-for (i = 0; i < len1 + len2; i++)                          result [i] = 0;                                            for (len1 = len1 - 1; len1 >= 0; len1--)                   {                                                          digit1 = s1[len] - '0';                                    carry = 0;                                                 for (len2 = len2 - 1; len2 >= 0; len2--)                   {                                                          digit2 = s2[len2] - '0';                                   carry += result[len1 + len2 + 1] + (digit1 * digit2);      result[len1 + len2 + 1] = carry % 10;
-carry /= 10;                                               }                                                          for (i = 0; i < len - 1; i++)                              {                                                          if (result[i])
-a = 1;
-if (a)                                                     _putchar(result[i] + '0');                                 }                                                          if (!a)                                                    _putchar('0');                                             _putchar('\n');                                            free(result);
-return (0);
+    if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
+        _error();
+
+    s1 = argv[1];
+    s2 = argv[2];
+    len1 = _strlen(s1);
+    len2 = _strlen(s2);
+    len = len1 + len2 + 1;
+    result = malloc(sizeof(int) * len);
+    if (!result)
+        return (1);
+
+    for (i = 0; i < len1 + len2; i++)
+        result[i] = 0;
+
+    for (len1 = len1 - 1; len1 >= 0; len1--)
+    {
+        digit1 = s1[len1] - '0';
+        carry = 0;
+        for (len2 = len2 - 1; len2 >= 0; len2--)
+        {
+            digit2 = s2[len2] - '0';
+            carry += result[len1 + len2 + 1] + (digit1 * digit2);
+            result[len1 + len2 + 1] = carry % 10;
+            carry /= 10;
+        }
+        result[len1 + len2 + 1] += carry;
+    }
+
+    for (i = 0; i < len - 1; i++)
+    {
+        if (result[i])
+            a = 1;
+        if (a)
+            _putchar(result[i] + '0');
+    }
+
+    if (!a)
+        _putchar('0');
+    _putchar('\n');
+    free(result);
+    return (0);
 }
-}
+
