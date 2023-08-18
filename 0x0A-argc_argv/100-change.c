8c7924003df392954fda328aa1278bce3949d325
samuel8751 <samuelgithinji041@gmail.com>
2023-08-17 18:02:07 -0700

    Write a program that prints the minimum number of coins to make change for an amount of money.

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,32 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * main - prints min no coin to make a change
- * @argc: no commandline args
- * @argv: arrays that holds commandline args
- * Return: 0;
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: An array containing the command line arguments
+ *
+ * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
-int cents, mincoin = 0;
-if (argc == 1 || argc > 2)
-{
-printf("Error\n");
-return (2);
+	int cents, coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	while (cents >= 25)
+	{
+		cents -= 25;
+		coins++;
+	}
+	while (cents >= 10)
+	{
+		cents -= 10;
+		coins++;
+	}
+	while (cents >= 5)
+	{
+		cents -= 5;
+		coins++;
+	}
+	while (cents >= 2)
+	{
+		cents -= 2;
+		coins++;
+	}
+	while (cents >= 1)
+	{
+		cents -= 1;
+		coins++;
+	}
+
+	printf("%d\n", coins);
+
+	return (0);
 }
-cents = atoi(argv[1]);
-while (cents > 0)
-{
-if (cents >= 25)
-cents -= 25;
-else if (cents >= 10)
-cents -= 10;
-else if (cents >= 5)
-cents -= 5;
-else if (cents >= 2)
-cents -= 1;
-mncoin += 1;
-}
-printf("%d\n", mncoin);
-return (0);
-}
+
