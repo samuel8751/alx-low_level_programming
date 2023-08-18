7879c7205cab9efc477bd1841106b7c2f78e5f78
samuel8751 <samuelgithinji041@gmail.com>
2023-08-17 17:52:16 -0700

    Write a program that adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,27 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+
 /**
- * main - adding +ve number
- * @argc: number of commandline args
- * @argv: nuber of commsnd line vector
- * Return: 0 duccess
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: An array containing the command line arguments
+ *
+ * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
-int i, j, sum = 0;
-for (i = 0; i < argc; i++)
-{
-for (j = 0; argv[i][j] != '\0'; j++)
-{
-if (isdigit(argv[i][j]))
-{
-printf("Error\n");
-return (1);
+	int sum = 0, num;
+	int i, j;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		num = atoi(argv[i]);
+		sum += num;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
 }
-}
-sum += atoi(argv[i]);
-}
-printf("%d\n", sum);
-return (0);
-}
+
